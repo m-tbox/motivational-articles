15bed85c3db8d9c94711829e55f4dbd7901ef5c4
Locaided <locaided@example.com>
2022-05-12 17:57:22 +0500

    Call sign up login apis from client

--- client/src/components/Signup/index.tsx
+++ client/src/components/Signup/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Form, Row, Col } from "react-bootstrap";
+import { callSignupApi } from "../../services/api";
 import Button from "../Button"
+import { ErrorMessage } from "./styles";
 import ModalComponent from "../Modal";
 
 type Props = {
@@ -13,9 +15,10 @@ const Signup = ({ showModal, closeModal }: Props) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errorMsg, setErrorMsg] = useState("");
+    const [localErrorMsg, setLocalErrorMsg] = useState("");
+    const [formSubmitErrorMsg, setFormSubmitErrorMsg] = useState("");
 
-    const handleSignup = (event: any) => {
+    const handleSignup = async (event: any) => {
         event.preventDefault();
 
         const form = event.currentTarget;
@@ -23,17 +26,25 @@ const Signup = ({ showModal, closeModal }: Props) => {
         if (form.checkValidity() === false) {
 
             if (!password) {
-                setErrorMsg("This field is mandatory");
+                setLocalErrorMsg("This field is mandatory");
             }
             else if (password.length < 6) {
-                setErrorMsg("Password length should be at least 6 character");
+                setLocalErrorMsg("Password length should be at least 6 character");
             }
 
             setPassword("");
             event.stopPropagation();
         }
         else {
-            alert(3);
+
+            let signupResponse = await callSignupApi({ email, password });
+            console.log('ssss', signupResponse);
+
+            if (signupResponse?.errors?.length) {
+                setFormSubmitErrorMsg(signupResponse.errors[0].msg);
+                setValidated(true);
+            }
+
         }
 
         setValidated(true);
@@ -43,9 +54,7 @@ const Signup = ({ showModal, closeModal }: Props) => {
         setValidated(false);
         setPassword("");
         setEmail("");
-        setErrorMsg("");
-
-        console.log(password, email, errorMsg, validated);
+        setLocalErrorMsg("");
 
         closeModal();
     }
@@ -63,6 +72,7 @@ const Signup = ({ showModal, closeModal }: Props) => {
                         <Form.Control
                             required
                             type="email"
+                            value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
                         <Form.Control.Feedback type="invalid">
@@ -77,13 +87,21 @@ const Signup = ({ showModal, closeModal }: Props) => {
                             required
                             type="password"
                             minLength={6}
+                            value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
                         <Form.Control.Feedback type="invalid">
-                            {errorMsg}
+                            {localErrorMsg}
                         </Form.Control.Feedback>
                     </Form.Group>
                 </Row>
+
+                {
+                    formSubmitErrorMsg &&
+                    <ErrorMessage>
+                        {formSubmitErrorMsg}
+                    </ErrorMessage>
+                }
 
                 <Row className="mb-3">
                     <Form.Group as={Col} className="d-flex justify-content-end">
---
+++ client/src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://localhost:8080"
+
+export const API_URLS = {
+    login: `${BASE_URL}/auth/login`,
+    signup: `${BASE_URL}/auth/signup`
+}
